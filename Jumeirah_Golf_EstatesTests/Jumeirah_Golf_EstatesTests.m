f40173445c3c0031696bd786329cc6a3f48da279
Veeramani <veeramani@SubhaChithtras-Mac-mini.local>
2014-07-15 12:57:44 +0530

    Initial Commit

---
+++ Jumeirah_Golf_EstatesTests/Jumeirah_Golf_EstatesTests.m
@@ -0,0 +1,34 @@
+//
+//  Jumeirah_Golf_EstatesTests.m
+//  Jumeirah_Golf_EstatesTests
+//
+//  Created by Veeramani on 15/07/14.
+//  Copyright (c) 2014 JumeirahGolfEstates. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Jumeirah_Golf_EstatesTests : XCTestCase
+
+@end
+
+@implementation Jumeirah_Golf_EstatesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
